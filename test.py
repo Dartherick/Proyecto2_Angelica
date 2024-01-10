f1a0395a3213b48755b6be7c5d3cbb1c0ccfd323
AngelicaHerrera2212 <132586865+AngelicaHerrera2212@users.noreply.github.com>
2024-01-10 14:38:29 -0400

    Deleting everything

--- test.py
+++
@@ -1,9 +0,0 @@
-from SerialCom import SerialPortConnection
-import time
-
-serial_connection = SerialPortConnection("COM7", 9600)
-serial_connection.open_port("COM7", 9600)
-
-while(True):
-    serial_connection.ReceiveMessage()
-    time.sleep(0.5)
